2ec8cb1638d5a28e2942f2c45e8a96c53cd0434e
Nunuy Amalia <108647899+Nunuyy@users.noreply.github.com>
2022-11-29 07:42:48 +0700

    Update atas.php

--- application/views/core/atas.php
+++ application/views/core/atas.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="navbar navbar-expand-lg">
 
-    <h1 class="navbar-brand mr-auto" style="font-size: 16px; color: white;">Aplikasi Voting Ketua MPK dan Osis 2021/2022</h1>
+    <h1 class="navbar-brand mr-auto" style="font-size: 16px; color: white;">Aplikasi Voting Ketua MPK dan Osis 2022/2023</h1>
 
     <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
             <span class="navbar-toggler-icon"></span>
